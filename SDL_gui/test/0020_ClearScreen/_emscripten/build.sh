4dfd9498dae499028d9bce61fb17a3f0dcf6ff1f
Siharat Thammaya <siharat.th@gmail.com>
2016-02-19 03:28:20 +0700

    Finished all test project

---
+++ SDL_gui/test/0020_ClearScreen/_emscripten/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+emcc \
+../src/main.cpp \
+../../../SDL_gui/GUI_utils.cpp \
+-I../../../SDL_gui \
+-s USE_SDL=2 -s LEGACY_GL_EMULATION=1 \
+-o sdl2.html \
+--emrun \
+-Wall -std=c++14 -Wno-absolute-value \
+-Wl,--as-needed -Wl,--gc-sections \
+-s DISABLE_EXCEPTION_CATCHING=0
---
+++ SDL_gui/test/0100_EditText/_Android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/Jim/___SDL/SDL_gui/test/0100_EditText/_Android/bin/AndroidManifest.xml : \
+/Users/Jim/___SDL/SDL_gui/test/0100_EditText/_Android/AndroidManifest.xml \
